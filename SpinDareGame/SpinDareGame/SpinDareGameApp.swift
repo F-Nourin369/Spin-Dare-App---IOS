9f2b0beeb357821e72a31d0f644070e557ded230
Fathima Nourin S U <fathimanourin369@gmail.com>
2025-08-08 11:25:52 +0530

    Initial commit

---
+++ SpinDareGame/SpinDareGame/SpinDareGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinDareGameApp.swift
+//  SpinDareGame
+//
+//  Created by Fathima Nourin on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SpinDareGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpinDareGame/SpinDareGameTests/SpinDareGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpinDareGameTests.swift
+//  SpinDareGameTests
+//
+//  Created by Fathima Nourin on 08/08/25.
+//
+
+import Testing
+@testable import SpinDareGame
+
+struct SpinDareGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
